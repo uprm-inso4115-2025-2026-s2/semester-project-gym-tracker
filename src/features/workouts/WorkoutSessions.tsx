4081e120a7c5888b95603bb87f78ab608988b9d1
yandielhernandez <yandiel.hernandez@upr.edu>
2026-03-31 15:49:12 -0400

    Setter for workoutsessions table finished

--- src/features/workouts/WorkoutSessions.tsx
+++ src/features/workouts/WorkoutSessions.tsx
@@ -1,20 +1,15 @@
-/*    
-  inside main in app.tsx put:
-  <WorkoutSessions/>
-  and the following include as well:
-  import WorkoutSessions from "./features/workouts/WorkoutSessions";
-  to show json response of sessions table
-
-  .env.xxxx has to be properly setted up 
-*/
-
+//to test place this import on app.tsx import WorkoutSessions from "./features/workouts/WorkoutSessions";
+//and inside the return <WorkoutSessions />
+//this will make a button appear that pressing it should send data to the supabase
+//currently it wont work since theres no user (UUID) attatched to the data
 
 import { useEffect, useState } from "react";
-import { getWorkoutSessions } from "./workoutSessionsApi";
+import { getWorkoutSessions, createWorkoutSession } from "./workoutSessionsApi";
 
 export default function WorkoutSessions() {
   const [sessions, setSessions] = useState<any[]>([]);
   const [error, setError] = useState("");
+  console.log("WorkoutSessions rendered");
 
   useEffect(() => {
     async function loadSessions() {
@@ -31,12 +26,37 @@ export default function WorkoutSessions() {
     loadSessions();
   }, []);
 
+  // this will need to be changed when the page is actually working and we want for the user to manaully 
+  // write what data they want to log
+  async function handleAddSession() {
+    try {
+      setError("");
+
+      const newSession = await createWorkoutSession({
+        workout_type: "Chest Day",
+        duration_minutes: 60,
+        calories_burned: 300,
+        notes: "Test workout",
+      });
+
+      console.log("Inserted:", newSession);
+
+      const updated = await getWorkoutSessions();
+      setSessions(updated ?? []);
+    } catch (err: any) {
+      console.error(err);
+      setError(err.message);
+    }
+  }
+
+  // this is temporal as well, just a button to add the previous functions data to the supabase
   return (
     <div>
       <h2>Workout Sessions</h2>
 
-      {error && <p>Error: {error}</p>}
+      <button onClick={handleAddSession}>Add Test Session</button>
 
+      {error && <p>Error: {error}</p>}
       {sessions.length === 0 && !error && <p>No sessions found.</p>}
 
       {sessions.map((s) => (
